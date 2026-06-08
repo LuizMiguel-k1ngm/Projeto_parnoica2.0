472f7297d51398687535887fb3f98fe8bada4913
Luiz Miguel <luizmigueldasilvagomes@gmail.com>
2026-06-08 09:59:55 -0300

    add: validações

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,25 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login()
+    {
         return view('login');
     }
 
-      public function loginSubmit(Request $request){
+    public function loginSubmit(Request $request)
+    {
 
-     echo $request->input('text_login');
-     echo "<br>";
-     echo $request->input('text_password');
-     
+        //form validation
+        $request->validate(
+            [
+                'text_login' => 'required| email',
+                'text_password' => 'required| min:6| max:16'
+
+            ],
+            //error messages
+            [
+                'text_login.required' => 'Preenchimento obrigatório',
+                'text_login.email' => 'Deve ser um email válido',
+                'text_password.required' => 'Preenchimento obrigatório',
+                'text_password.min' => 'A senha deve conter pelo menos :min caracteres',
+                'text_password.max' => 'A senha deve conter pelo até :max caracteres'
+
+            ]
+
+        );
+
+        // get user input
+
+        $login = $request->input('text_login');
+        $password = $request->input('text_password');
+
+        echo 'OK';
     }
 
 
 
 
-    public function logout(){
+    public function logout()
+    {
         echo 'logout';
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,21 +2,42 @@
 @section('content')
 
 
+<style>
+  #error {
+    justify-content: center;
+    align-items: center;
+    margin: 38%;
+  }
+</style>
 
 <div>
 
   <form class=" mt-5 text-center" action="/loginSubmit" method="post">
-  @csrf
+    @csrf
     <div class="mb-2">
       <label for="text_login" class="form-label ">LOGIN</label><br>
-      <input type="text" class="form-control-lg" name="text_login" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <input type="text" class="form-control-lg" name="text_login" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{old('text_login')}}">
       <div id="emailHelp" class="form-text text-info"></div>
+      {{-- show error --}}
+      @error('text_login')
+      <div class="class text-danger">{{$message}}</div>
+      @enderror
+
+
     </div>
     <div class="mb-3">
       <label for="text_password" class="form-label">SENHA</label><br>
-      <input type="password" class="form-control-lg" name="text_password" id="exampleInputPassword1">
+      <input type="password" class="form-control-lg" name="text_password" id="exampleInputPassword1" value="{{old('text_password')}}">
+      
+      {{-- show error --}}
+      @error('text_password')
+      <div class="class text-danger">{{$message}}</div>
+      @enderror
+
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
+
+
   </form>
 
 </div>
