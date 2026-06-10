0ef225a634cd9458692e87260ebc8c74045c9223
Luiz Miguel <luizmigueldasilvagomes@gmail.com>
2026-06-10 14:41:40 -0300

    remove: remove some trash

--- resources/views/layout/main_layout.blade.php
+++ resources/views/layout/main_layout.blade.php
@@ -30,17 +30,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <!-- <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="login">Home</a>
-                    </li> -->
-                    <!-- <li class="nav-item">
-                        <a class="nav-link" href="#
-                        ">Link</a>
-                    </li> -->
-                    <!-- <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a> -->
+                    
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="#">Action</a></li>
                         <li><a class="dropdown-item" href="#">Another action</a></li>
@@ -50,9 +40,6 @@
                         <li><a class="dropdown-item" href="#">Something else here</a></li>
                     </ul>
                     </li>
-                    <!-- <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li> -->
                 </ul>
 
                 <a href="" class="text-info me-2 d-flex align-items-center gap-2 me-5">
